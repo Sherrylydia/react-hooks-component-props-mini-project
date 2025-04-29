b486b31f4e055c3dc64e7b37174d540d7ad88b96
LYDIA <lydiakinuthia91@gmail.com>
2025-04-29 22:12:59 +0300

    passing props and children components

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -5,7 +5,6 @@ import App from "../components/App";
 test("renders the correct child components", () => {
   const { container } = render(<App />);
   expect(container.querySelector(".App")).toBeInTheDocument();
-  expect(container.querySelector(".App header")).toBeInTheDocument();
   expect(container.querySelector(".App aside")).toBeInTheDocument();
   expect(container.querySelector(".App main")).toBeInTheDocument();
 });
